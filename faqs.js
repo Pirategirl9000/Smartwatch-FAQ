affc024471da1b0f545446b66a8e3ce9bb4f6504
Violet French <BraedynFrench@gmail.com>
2026-03-03 14:24:32 -0600

    Added top level documentation

---
+++ faqs.js
@@ -0,0 +1,7 @@
+/**
+ * This program displays frequently asked questions about a watch
+ * with drop down menus that manipulate the dom images
+ * @author Violet French - https://github.com/Pirategirl9000
+ * @author Sarah Fenton - https://github.com/sarahfenton204
+ * @link https://github.com/Pirategirl9000/Smartwatch-FAQ
+ */
